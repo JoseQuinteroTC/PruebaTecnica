7d9644dc17eaa63eeeeb9907e56c062a516f98a7
Jose Quintero <cuellojose29@gmail.com>
2024-06-16 19:36:28 -0500

    Configuracion inicial

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      <List/>
+      </header>
+    </div>
+  );
+}
+
+function List() {
+  return (
+    <div className="List">
+      
+    </div>
+  );
+}
+
+
+export default App;

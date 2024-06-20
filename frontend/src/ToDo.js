8f6f033e4d8659d689025ae420c3fc3e2a9260e6
Jose Quintero <cuellojose29@gmail.com>
2024-06-19 22:14:10 -0500

    mas css y boton de completado

--- frontend/src/ToDo.js
+++ frontend/src/ToDo.js
@@ -5,6 +5,7 @@ function ToDo({ task, deleteTask, editTask }) {
     <li style={{ backgroundColor: task.color }}>
       <h3>{task.title}</h3>
       <p>{task.description}</p>
+      <button onClick={editTask}>Completado</button>
       <button onClick={editTask}>Editar</button>
       <button onClick={deleteTask}>Eliminar</button>
     </li>
